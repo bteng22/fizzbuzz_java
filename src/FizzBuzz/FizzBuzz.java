d0e54810fb704c8c364267113f9108df77f4a119
Brandon Teng <bteng22@gmail.com>
2014-12-11 22:41:23 -0600

    finish solution

--- src/FizzBuzz/FizzBuzz.java
+++ src/FizzBuzz/FizzBuzz.java
@@ -5,7 +5,23 @@ package FizzBuzz;
  */
 public class FizzBuzz {
     public String evaluate(int number) {
+        if (isMultipleOf3(number) && isMultipleOf5(number)) {
+            return "FizzBuzz";
+        }
+        if (isMultipleOf3(number)) {
+            return "Fizz";
+        } else if (isMultipleOf5(number)) {
+            return "Buzz";
+        }
         return String.valueOf(number);
+    }
+
+    private boolean isMultipleOf5(int number) {
+        return number % 5 == 0;
+    }
+
+    private boolean isMultipleOf3(int number) {
+        return number % 3 == 0;
     }
 }
 
--- test/FizzBuzzTest.java
+++ test/FizzBuzzTest.java
@@ -25,4 +25,34 @@ public class FizzBuzzTest {
     public void twoReturnsTwo() {
         assertEquals("2", fizzBuzz.evaluate(2));
     }
+
+    @Test
+    public void threeReturnsFizz() {
+        assertEquals("Fizz", fizzBuzz.evaluate(3));
+    }
+
+    @Test
+    public void fiveReturnsBuzz() {
+        assertEquals("Buzz", fizzBuzz.evaluate(5));
+    }
+
+    @Test
+    public void sixReturnsFizz() {
+        assertEquals("Fizz", fizzBuzz.evaluate(6));
+    }
+
+    @Test
+    public void tenReturnsBuzz() {
+        assertEquals("Buzz", fizzBuzz.evaluate(10));
+    }
+
+    @Test
+    public void fifteenReturnsFizzBuzz() {
+        assertEquals("FizzBuzz", fizzBuzz.evaluate(15));
+    }
+
+    @Test
+    public void thurtyReturnsFizzBuzz() {
+        assertEquals("FizzBuzz", fizzBuzz.evaluate(30));
+    }
 }
