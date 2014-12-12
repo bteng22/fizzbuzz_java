630d0abb87756807b139095554e9d871c96292d1
Brandon Teng <bteng22@gmail.com>
2014-12-11 22:28:40 -0600

    test one returns one

---
+++ src/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,11 @@
+package FizzBuzz;
+
+/**
+ * Created by brandonteng on 12/11/14.
+ */
+public class FizzBuzz {
+    public String evaluate(int number) {
+        return "1";
+    }
+}
+
---
+++ test/FizzBuzzTest.java
@@ -0,0 +1,24 @@
+import FizzBuzz.FizzBuzz;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by brandonteng on 12/11/14.
+ */
+public class FizzBuzzTest {
+
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() throws Exception {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    public void oneReturnsOne() {
+        assertEquals("1", fizzBuzz.evaluate(1));
+    }
+
+}
